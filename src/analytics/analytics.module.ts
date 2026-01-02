463aeabcdacdfe7f3058edd548d7bbb965c337b4
ragul <ragulhp2704@gmail.com>
2026-01-02 10:16:33 +0530

    Update analytics.module.ts

--- src/analytics/analytics.module.ts
+++ src/analytics/analytics.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AnalyticsService } from './analytics.service';
 import { AnalyticsController } from './analytics.controller';
+import { DbModule } from '../db/drizzle.module';
 
 @Module({
+  imports: [DbModule],
   providers: [AnalyticsService],
   controllers: [AnalyticsController],
   exports: [AnalyticsService],
